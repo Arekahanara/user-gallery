2b3be56aa635ea6517934154eb911940671be0d8
Alexander Ingwersen <alexander-ingwersen@live.com>
2021-12-18 15:04:29 +0100

    add eslint

---
+++ client/.eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/no-deprecated-slot-attribute': 'off',
+    'vue/multi-word-component-names': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
+}
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
-  <p>Hallo</p>
+        <p>Hallo</p>
       </div>
     </div>
   </div>
